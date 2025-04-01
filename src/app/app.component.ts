2cb8c807880985e0dc59d68711d4be4f9721a4ce
Wataru.Kasahara <w.kasahara@emotion-tech.co.jp>
2025-04-01 17:15:12 +0900

    feat: 複数パターンのフォームを配置

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
+import { TextFieldModule } from '@angular/cdk/text-field';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, TextFieldModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng-sample-202504';
+  form = new FormGroup({
+    text: new FormControl('', { nonNullable: true }),
+  });
 }
